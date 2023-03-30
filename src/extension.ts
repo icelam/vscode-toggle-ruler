8ee7d1d154a62172f85a609667313cfcaf4668be
Ice Lam <pinky.yy.lam@gmail.com>
2023-03-30 19:49:04 +0800

    refactor: fix indentation

--- src/extension.ts
+++ src/extension.ts
@@ -8,63 +8,63 @@ const RULER_POSITION_SETTING_KEY = 'toggleRuler.rulerPosition';
 const IS_SETTING_GLOBAL = true;
 
 async function toggleSetting() {
-	try {
-		let config = vscode.workspace.getConfiguration();
+  try {
+    let config = vscode.workspace.getConfiguration();
 
-		const rulerPosition = config.get(RULER_POSITION_SETTING_KEY);
-		if (!rulerPosition) {
-			throw new Error('Ruler position is not set');
-		}
+    const rulerPosition = config.get(RULER_POSITION_SETTING_KEY);
+    if (!rulerPosition) {
+      throw new Error('Ruler position is not set');
+    }
 
-		const currentRulerSetting = config.get(RULER_VSCODE_SETTING_KEY);
-		if (
-			!currentRulerSetting
-			|| !Array.isArray(currentRulerSetting)
-			|| currentRulerSetting.length === 0
-		) {
-			await config.update(RULER_VSCODE_SETTING_KEY, [rulerPosition], IS_SETTING_GLOBAL);
-		} else {
-			await config.update(RULER_VSCODE_SETTING_KEY, [], IS_SETTING_GLOBAL);
-		}
-	} catch (error) {
-		vscode.window.showErrorMessage(
-			`Failed to toggle editor ruler. Reason: ${(error as Error).message}`
-		);
-	}
+    const currentRulerSetting = config.get(RULER_VSCODE_SETTING_KEY);
+    if (
+      !currentRulerSetting
+      || !Array.isArray(currentRulerSetting)
+      || currentRulerSetting.length === 0
+    ) {
+      await config.update(RULER_VSCODE_SETTING_KEY, [rulerPosition], IS_SETTING_GLOBAL);
+    } else {
+      await config.update(RULER_VSCODE_SETTING_KEY, [], IS_SETTING_GLOBAL);
+    }
+  } catch (error) {
+    vscode.window.showErrorMessage(
+      `Failed to toggle editor ruler. Reason: ${(error as Error).message}`
+    );
+  }
 }
 
 async function updateRulerPosition() {
-	try {
-		let config = vscode.workspace.getConfiguration();
+  try {
+    let config = vscode.workspace.getConfiguration();
 
-		const rulerPosition = config.get(RULER_POSITION_SETTING_KEY);
-		if (!rulerPosition) {
-			throw new Error('Ruler position is not set');
-		}
+    const rulerPosition = config.get(RULER_POSITION_SETTING_KEY);
+    if (!rulerPosition) {
+      throw new Error('Ruler position is not set');
+    }
 
-		const currentRulerSetting = config.get(RULER_VSCODE_SETTING_KEY);
-		if (Array.isArray(currentRulerSetting) && currentRulerSetting.length > 0) {
-			await config.update(RULER_VSCODE_SETTING_KEY, [rulerPosition], IS_SETTING_GLOBAL);
-		}
-	} catch (error) {
-		vscode.window.showErrorMessage(
-			`Failed to update position of editor ruler. Reason: ${(error as Error).message}`
-		);
-	}
+    const currentRulerSetting = config.get(RULER_VSCODE_SETTING_KEY);
+    if (Array.isArray(currentRulerSetting) && currentRulerSetting.length > 0) {
+      await config.update(RULER_VSCODE_SETTING_KEY, [rulerPosition], IS_SETTING_GLOBAL);
+    }
+  } catch (error) {
+    vscode.window.showErrorMessage(
+      `Failed to update position of editor ruler. Reason: ${(error as Error).message}`
+    );
+  }
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Extension "Toggle Ruler" is activated.');
-	let commandDisposable = vscode.commands.registerCommand('toggleRuler.toggle', toggleSetting);
-	context.subscriptions.push(commandDisposable);
+  console.log('Extension "Toggle Ruler" is activated.');
+  let commandDisposable = vscode.commands.registerCommand('toggleRuler.toggle', toggleSetting);
+  context.subscriptions.push(commandDisposable);
 
-	let cofigChangeDisposable = vscode.workspace.onDidChangeConfiguration(e => {
-		vscode.workspace.onDidChangeConfiguration(updateRulerPosition);
-	});
+  let cofigChangeDisposable = vscode.workspace.onDidChangeConfiguration(e => {
+    vscode.workspace.onDidChangeConfiguration(updateRulerPosition);
+  });
 
-	context.subscriptions.push(cofigChangeDisposable);
+  context.subscriptions.push(cofigChangeDisposable);
 }
 
 export function deactivate() {
-	console.log('Extension "Toggle Ruler" is deactivated.');
+  console.log('Extension "Toggle Ruler" is deactivated.');
 }
